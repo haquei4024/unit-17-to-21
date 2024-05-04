964e4de641ef4832bec3c75b5a0a7d0e90a7938f
haquei4024 <m.irfanhq1@gmail.com>
2024-05-04 15:41:57 -0400

    Responsive Navbar (Part 2)
    Added the menu on click interaction for mobile

--- src/Nav/Navbar.js
+++ src/Nav/Navbar.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css'
-import { MagnifyingGlass} from 'phosphor-react';
-import { Dropdown } from './Dropdown';
+import { MagnifyingGlass, List} from 'phosphor-react';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
   return (
     <nav>
-        <Link to="/" className='Title'>
-            GH
-        </Link>
-        <Dropdown />
-        <ul className='Pages'>
-            <li>
-                <NavLink to="/home">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/stories">Stories</NavLink>
-            </li>
-            <li>
-                <NavLink to="/calendar">Calendar</NavLink>
-            </li>
-        </ul>
-        <ul className='SignUps'>
-            <li><MagnifyingGlass size={20} /></li>
-            <li className='ButtonSign'> SIGN UP</li>
+        <div className='Desktop'>    
+            <Link to="/" className='Title'>
+                GH
+            </Link>
+            <div className='HamMenu' onClick={() => {setMenuOpen(!menuOpen)}}>
+                <List size={30}/>
+            </div>
+            <ul className='Pages'>
+                <li>
+                    <NavLink to="/home">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/stories">Stories</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/calendar">Calendar</NavLink>
+                </li>
+            </ul>
+            <ul className='SignUps'>
+                <li><MagnifyingGlass size={20} /></li>
+                <li className='ButtonSign'> SIGN UP</li>
+            </ul>
+        </div>
+        <ul className={menuOpen ? "MobileMenuOpen" : "MobileMenuClose"}>
+            <li>Home</li>
+            <li>Stories</li>
+            <li>Calendar</li>
         </ul>
     </nav>
   )
